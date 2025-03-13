77b2d14ab3c9dd00285152923154ddd56fda6a79
MogacBogdan <constantin-bogdan.moga@student.tuiasi.ro>
2025-03-13 21:15:01 +0200

    Delete files out of L1 repository

--- generate-keypair.ts
+++
@@ -1,7 +0,0 @@
-import {Keypair} from "@solana/web3.js";
-
-const keypair = Keypair.generate();
-
-console.log("Keypair generated successfully!");
-console.log("Public key", keypair.publicKey.toBase58());
-console.log("SecretKey", keypair.secretKey)
